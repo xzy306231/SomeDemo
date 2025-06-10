f3dbdeccad2f06266e175282c1f98b2eb3f2e18c
xuzhiyang <xuzhiyang@richisland.com>
2025-06-10 08:39:14 +0800

    YamlDotnet

---
+++ YamlDotNetDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization;
+
+namespace YamlDotNetDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new Dictionary<string, int>();
+            var text = File.ReadAllLines($"{AppDomain.CurrentDomain.BaseDirectory}/config.txt");
+            foreach (var item in text)
+            {
+                var len = item.Split("=");
+                dict.Add(len[0], int.Parse(len[1]));
+            }
+
+            var pathList = new List<string> { "/richisland/gsdd", "/richisland/nlgsdd", "/richisland/ytyh" };
+            var serializer = new SerializerBuilder().WithNamingConvention(UnderscoredNamingConvention.Instance).Build();
+            var deserializer = new DeserializerBuilder().WithNamingConvention(UnderscoredNamingConvention.Instance).Build();
+
+            foreach (var item in pathList)
+            {
+                var ymlFile = Path.Combine($"{AppDomain.CurrentDomain.BaseDirectory}{item}", "docker-compose.yaml");
+                if (File.Exists(ymlFile))
+                {
+                    var ymlContent = File.ReadAllText(ymlFile);
+                    var buildConfig = deserializer.Deserialize<DockerComposeConfig>(ymlContent);
+                    if (buildConfig != null)
+                    {
+                        if (buildConfig.Version == null)
+                            buildConfig.Version = "''3''"; // 设置默认版本
+                        foreach (var ser in buildConfig.Services)
+                        {
+                            if (ser.Value.Ports == null)
+                            {
+                                ser.Value.Ports = new List<string> { $"{dict[ser.Key]}:{dict[ser.Key]}" };
+                            }
+                            if (ser.Value.Logging == null)
+                                ser.Value.Logging = new Dictionary<string, string>
+                                {
+                                    { "driver", "none" }
+                                };
+                        }
+                        if (buildConfig.Volumes == null)
+                            buildConfig.Volumes = new Dictionary<string, Volume>();
+                        var newYamlContent = serializer.Serialize(buildConfig);
+
+                        Console.WriteLine($"正在处理{item}/docker-compose.yaml");
+                        if (!Directory.Exists($"{AppDomain.CurrentDomain.BaseDirectory}/new/{item}"))
+                            Directory.CreateDirectory($"{AppDomain.CurrentDomain.BaseDirectory}/new/{item}");
+
+                        File.WriteAllText($"{AppDomain.CurrentDomain.BaseDirectory}/new/{item}/docker-compose.yaml", newYamlContent);
+                        Thread.Sleep(1000);
+                    }
+                }
+            }
+
+            Console.WriteLine("处理成功");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ YamlDotNetDemo/YamlDto.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+
+namespace YamlDotNetDemo
+{
+    public class DockerComposeConfig
+    {
+        [YamlMember(Alias = "version")]
+        public string Version { get; set; }
+
+        [YamlMember(Alias = "services")]
+        public Dictionary<string, Service> Services { get; set; }
+
+        [YamlMember(Alias = "networks")]
+        public Dictionary<string, Network> Networks { get; set; }
+
+        [YamlMember(Alias = "volumes")]
+        public Dictionary<string, Volume> Volumes { get; set; }
+    }
+
+    public class Service
+    {
+        [YamlMember(Alias = "container_name")]
+        public string ContainerName { get; set; }
+
+        [YamlMember(Alias = "image")]
+        public string Image { get; set; }
+
+        [YamlMember(Alias = "restart")]
+        public string Restart { get; set; } = "always";
+
+        [YamlMember(Alias = "privileged")]
+        public bool Privileged { get; set; }
+
+        [YamlMember(Alias = "volumes")]
+        public List<string> Volumes { get; set; }
+
+        [YamlMember(Alias = "ports")]
+        public List<string> Ports { get; set; }
+
+        [YamlMember(Alias = "environment")]
+        public Dictionary<string, string> Environment { get; set; }
+        public Dictionary<string, string> Logging { get; set; }
+
+        [YamlMember(Alias = "networks")]
+        public Dictionary<string, ServiceNetwork> Networks { get; set; }
+
+    }
+
+    public class ServiceNetwork
+    {
+       // [YamlMember(Alias = "ipv4_address")]
+        public string Ipv4_address { get; set; }
+    }
+
+    public class Network
+    {
+        [YamlMember(Alias = "external")]
+        public External External { get; set; }
+    }
+
+    public class External
+    {
+        [YamlMember(Alias = "name")]
+        public string Name { get; set; }
+    }
+
+    public class Volume
+    {
+        // 空对象，用于处理 volumes: {}
+    }
+
+    public class Logging
+    {
+        [YamlMember(Alias = "driver")]
+        public string Driver { get; set; }
+    }
+}
